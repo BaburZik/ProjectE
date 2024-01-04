3689298eb0e6ff02c210480f8bc3112659278301
babur <babur@BABURlaptop.fios-router.home>
2024-01-03 21:08:49 -0500

    Initial Commit

---
+++ src/E/DemoE.java
@@ -0,0 +1,11 @@
+package E;
+
+public class DemoE {
+	public static void main(String[] args) {
+		System.out.println("friday");
+		
+		System.out.println("friday");
+		System.out.println("friday");
+	}
+
+}
